40597dce1093b5912e14a8a9c00eaed14594e9ca
Samanta Fluture <samantafluture@gmail.com>
2021-09-12 00:43:23 -0300

    refactor map component to get current local

--- src/app/components/shared/map/map.component.ts
+++ src/app/components/shared/map/map.component.ts
@@ -6,17 +6,17 @@ import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
-  title = 'angular-gmap';
   @ViewChild('mapContainer', { static: false }) gmap!: ElementRef;
   map!: google.maps.Map;
-  lat = 40.73061;
-  lng = -73.935242;
-
+  lat = 0;
+  lng = 0;
+  zoom = 12;
   coordinates = new google.maps.LatLng(this.lat, this.lng);
+  infoWindow = new google.maps.InfoWindow();
 
   mapOptions: google.maps.MapOptions = {
     center: this.coordinates,
-    zoom: 8,
+    zoom: 12,
   };
 
   marker = new google.maps.Marker({
@@ -26,10 +26,27 @@ export class MapComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.mapInitializer();
+    this.setCurrentLocation();
   }
 
   mapInitializer() {
     this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
     this.marker.setMap(this.map);
   }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.infoWindow.setPosition(pos);
+          this.infoWindow.setContent("Location found.");
+          this.infoWindow.open(this.map);
+          this.map.setCenter(pos);
+      });
+    }
+  }
+
 }
